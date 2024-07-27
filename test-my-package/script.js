f002dc9279997968221b9d1b51a262da50456538
Karthik <karthikprem1694@gmail.com>
2024-07-27 12:56:53 +0530

    tested utils

---
+++ test-my-package/script.js
@@ -0,0 +1,30 @@
+// index.js
+
+import {
+    reverseString,
+    capitalizeFirstLetter,
+    toCamelCase,
+    countWords,
+    isPalindrome,
+    randomString,
+    toTitleCase,
+    uniqueArray,
+    shuffleArray,
+    maxArray,
+    minArray,
+    removeDuplicates,
+    chunkArray,
+    flattenArray,
+    extractEmailDomain,
+    averageArray,
+    medianArray,
+    intersectArrays,
+    removeWhitespace,
+    countOccurrences
+    
+} from '@prem/utils';
+const nestedArray = [1, 2, 3, 5];
+const arr = [2,5,6];
+const str = "bro this is class one and one and one";
+const sub = "one";
+console.log(countOccurrences(str,sub)); // 'olleh'
